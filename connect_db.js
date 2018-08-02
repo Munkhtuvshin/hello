85c45dcef8aa9b0a3afb3e9d7aa13fcacf96ea22
dambii say77 <saytan77saytan@gmail.com>
2018-08-02 18:20:46 +0800

    fdf

--- connect_db.js
+++ connect_db.js
@@ -3,8 +3,8 @@ const mongoose  = require('mongoose')
 mongoose.Promise = global.Promise
 
 async function run() {
-	await mongoose.connect("mongodb://localhost/demo", {
-		//useMongoClient:true
+	await mongoose.connect("mongodb://localhost:27017/demo", {
+		useNewUrlParser: true
 	});
 }
 
--- demo1.js
+++ demo1.js
@@ -6,41 +6,58 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 var Event = require("./event.js");
 var db_connect = require("./connect_db.js");
 
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
-app.use(function (req, res, next){
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-*, Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-})
-  
-  //var silence = new Event({ name: 'Silence' });
-  Event.create({
-    title : "mahesh",
-    cover_url : '',
-    coordinate:{
-        lat:47.78963221880257,
-        lng:107.38140106201172,  
-      },
-    start_at:'2018-08-08',
-    end_at:'2018-08-08',
-  }, (err, silence) => {
-    console.log(silence);
+  app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+  })); 
+
+  app.use(function (req, res, next){
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-*, Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
   })
 
+  app.get('/event', function (req, res) {
+    Event
+    .find({})
+    .limit(10)
+    .exec(function(err, events) {
+      return res.json(events);  
+    });
+  })
 
+  app.post('/event', function (req, res) {
+    console.log('nemeh')
+    Event.create(req.body, (err, event) => {
+      console.log(event);
+      return res.json(event);
+    })
+  })
 
+  app.delete('/event/:id', function (req, res) {
+    Event
+    .findOne({ _id:req.params.id })
+    .remove()
+    .exec(function(err, event){
+      return res.json(req.body);
+    });
+  })
 
+  app.put('/event/:id', function (req, res) {
+    Event.
+    findOneAndUpdate( { _id:req.params.id }, { $set: req.body }, 
+      function(err, event){
+        return res.json(event);
+      }
+    )
+  })
 
+  var server = app.listen(8081, function () {
 
-var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
 
-  var host = server.address().address
-  var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
 
-  console.log("Example app listening at http://%s:%s", host, port)
-
-})
+  })
